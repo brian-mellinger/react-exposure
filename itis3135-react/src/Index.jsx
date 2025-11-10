e142925aaa825cedf1468031fbc00f18f4fff08f
Brian <bjmellin@charlotte.edu>
2025-11-09 23:35:51 -0500

    Update Index.jsx

--- itis3135-react/src/Index.jsx
+++ itis3135-react/src/Index.jsx
@@ -6,7 +6,7 @@ export default function Index() {
     <script src="https://lint.page/kit/4d0fe3.js" crossorigin="anonymous"></script>
     <main>
         <h2>Home</h2>
-        <p>Hello, you've made it to my ITIS 3135 homepage.</p>
+        <p>Hello, you've made it to my ITIS 3135 homepage .</p>
     </main>
         </>
     );
